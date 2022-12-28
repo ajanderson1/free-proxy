293cafffcd8ad87438ee0ed2403c5652b275306d
ajanderson1 <79423420+ajanderson1@users.noreply.github.com>
2022-12-28 08:06:12 +0000

    .

---
+++ src/free_proxy/get_first_valid_proxy.py
@@ -0,0 +1,17 @@
+from . import validate_proxy
+import random
+
+import logging
+log = logging.getLogger(__name__)
+
+def get_first_valid_proxy(proxies) -> dict:
+    """ ✅  Returns the first valid proxy from the list of passed in proxies """
+    if proxies is None:
+        log.error("No proxies passed in")
+        raise ValueError("No proxies passed in")
+    random.shuffle(proxies)
+    for proxy in proxies:
+        if validate_proxy(proxy):
+            return proxy
+    log.error("No *valid* proxies found")
+    raise ValueError("No *valid* proxies found")
